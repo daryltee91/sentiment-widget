0647f81fc7668c0bca0acd716d183f3b39add779
daryltee91 <daryltee@gmail.com>
2026-02-04 12:54:08 +0800

    Update ratingchips test case

--- src/components/RatingChips/ratingchips.spec.tsx
+++ src/components/RatingChips/ratingchips.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { act, render, screen } from "@testing-library/react";
 import { RatingChips } from ".";
 import { describe, expect, it } from "vitest";
 
@@ -14,5 +14,14 @@ describe("Rating Chips component", () => {
     for (let i = 1; i < 6; i++) {
       expect(screen.getByTestId(`rating-chips-${i}`)).toBeVisible();
     }
+  });
+
+  it("should have a `selected` class when a chip is selected", async () => {
+    await act(() => {
+      render(<RatingChips rating={1} error="" setRating={() => {}} />);
+    });
+
+    expect(screen.getByTestId(`rating-chips-1`)).toBeVisible();
+    expect(screen.getByTestId(`rating-chips-1`)).toHaveClass("selected");
   });
 });
